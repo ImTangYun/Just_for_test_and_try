69986a01f044d24488b04b5827d2d657ea977c97
lenovo <candy@ubuntu.ubuntu-domain>
2014-06-28 16:26:48 +0800

    modify some files

--- test_http_server/depend/epoll/transmission.cpp
+++ test_http_server/depend/epoll/transmission.cpp
@@ -3,6 +3,8 @@
  *filename:transmission.h
  *
  */
+#include <stdio.h>
+#include <unistd.h>
 #include "transmission.h"
 #include "Cthread.h"
 
@@ -11,9 +13,29 @@ namespace myspace
 using cthread::CThread;
 Transmission::Transmission(SocketContent* listen_content, EventPoller* event_poller)
 {
-	trans_thread_ = new CThread;
+	// trans_thread_ = new CThread;
 }
 Transmission::~Transmission()
 {
+}
+bool Transmission::Start()
+{
+	// int* x = new int(0);
+	// trans_thread_->Start(this, (void*)x);
+	return true;
+}
+bool Transmission::Stop()
+{
+	return true;
+}
+void Transmission::EventLoop()
+{
+	printf("this is the main loop of transmission\n");
+	sleep(1);
+	EventLoop();
+}
+void Transmission::Run(CThread* cthread, void* args)
+{
+	EventLoop();
 }
 }
--- test_http_server/depend/epoll/transmission.h
+++ test_http_server/depend/epoll/transmission.h
@@ -22,6 +22,7 @@ public:
 	void EventLoop();
 	virtual void Run(CThread* cthread, void* args);
 private:
+	bool running_;
 	CThread trans_thread_;
 	EventPoller* event_poller_;
 	SocketContent* listen_content_;
