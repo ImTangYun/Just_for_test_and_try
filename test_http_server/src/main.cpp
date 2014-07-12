9fd72d4fd6f35a8cf9cf3ad8db63b390a0c04f3c
lenovo <candy@ubuntu.ubuntu-domain>
2014-07-12 16:26:57 +0800

    changed main.cpp

--- test_http_server/src/main.cpp
+++ test_http_server/src/main.cpp
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "Work_flow.h"
 #include "communicate.h"
 #include "http_socket_handler.h"
-#include "Cthread.h"
 #include "http_conf.h"
-using cthread::WorkFlow;
 using http::HttpSocketHandler;
 using myspace::Communicate;
 using http::HttpConfig;
@@ -15,13 +12,7 @@ int main()
 	HttpSocketHandler* listen_handler = new HttpSocketHandler(http_config.Port());
 	Communicate* communicate = new Communicate(listen_handler);
 	communicate->Start();
-	WorkFlow work;
-	work.Init(4);
-	work.Start();
-	sleep(1);
-	sleep(100);
-	work.Stop();
-	sleep(5000);
+	while (true) sleep(10);
 	return 0;
 }
 
