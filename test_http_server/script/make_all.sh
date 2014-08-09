9c1e85254e4d833895bf16ee86e5350020bd6a0f
yuntang <1282823258@qq.com>
2014-08-09 17:31:47 +0800

    add start.sh and stop.sh

--- test_http_server/script/make_all.sh
+++ test_http_server/script/make_all.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 cd ../depend/epoll
 cd ../makedir
-./link.sh
+#./link.sh
+cp ../thread/* ./                                                                                                 
+make clean
+make
+cp ../epoll/* ./
+make
+make install
+
 cd ../../src
 make clean
 make
+rm *.o
 cd ../depend/makedir
 rm Makefile
 rm *.o
