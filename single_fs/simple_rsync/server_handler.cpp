11da6d1e3981ff20b0a3ee361658a68444a2d7af
tangyun <1282823258@qq.com>
2015-12-03 23:44:06 +0800

    configure.in of net

--- single_fs/simple_rsync/server_handler.cpp
+++ single_fs/simple_rsync/server_handler.cpp
@@ -38,47 +38,6 @@ int ServerHandler::EchoFromServer(Packet* packet)
             task_node->req_, path_length, task_node->path_->c_str());
     task_queue_->Push(task_node);
     return 0;
-
-    RequestType reqest_type = (RequestType)ntohl(((int*)head)[0]);
-    WLOG(DEBUG, "head length: %d, reqest_type_: %d", head_length, reqest_type);
-
-    char* data = packet->data();
-    EndPoint* end_point = packet->end_point();
-    if (reqest_type == GET_CHUNK) {
-        data[packet->data_length()] = '\0';
-        int64_t file_id = (int64_t)atoi(data);
-        if (file_id <= 0) {
-            file_id = 10;
-        }
-        WLOG(DEBUG, "file_id=%lu", file_id);
-        delete [] packet->data();
-
-        int64_t file_length = 0;
-        Response response = OK;
-        if (file_length < 0) {
-            response = FILE_DID_NOT_EXIST;
-            WLOG(WARN, "file of id %lu did not exist", file_id);
-            file_length = 4;
-        }
-        char *buf = new char[file_length + 2];
-        buf[file_length] = '\0';
-        int64_t ret = 0;
-        if (ret < 0) {
-            if (response != OK) {
-                WLOG(ERROR, "read failed!");
-            }
-        }
-        WLOG(DEBUG, "send:\n%s\nlength:%lu", buf, file_length);
-
-        int* head = new int[2];
-        head[0] = htonl(response);
-        packet->set_head((char*)head, 2 * sizeof(int));
-        packet->set_packet(buf, (uint32_t) file_length);
-        StreamSocketContext* socket_context = end_point->socket_context();
-        socket_context->AsyncSendPacket(packet);
-    } else if (reqest_type == GET_CHUNK) {
-    }
-    return 0;
 }
 int ServerHandler::OnReceived(Packet* packet, EndPoint* end_point)
 {
